c64d2e260c181e2788c80e1e35d8c1760fd388ae
TXH7SP <txh7sp@neptun.gdf.hu>
2022-03-10 10:49:55 +0100

    Tantargy class created

---
+++ src/elosztottalkgit/Tantargy.java
@@ -0,0 +1,14 @@
+package elosztottalkgit;
+
+public class Tantargy {
+    private int Azonosito;
+
+    public Tantargy(String nev) {
+        
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
